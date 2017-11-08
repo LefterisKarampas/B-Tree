1334a68397b1bf0b936eee08a12527b338fa93cf
LefterisKarampas <mr.lef_21113@hotmail.com>
2017-11-08 21:39:35 +0200

    Global structures & Create_Index

--- AM_64/src/AM.c
+++ AM_64/src/AM.c
@@ -1,17 +1,78 @@
 #include "AM.h"
+#include "bf.h"
+#include "Global_Struct.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 int AM_errno = AME_OK;
 
+open_files **Open_Files = NULL;
+scan_files **Scan_Files = NULL;
+
+
+/*Initialize the global structures and BF*/
 void AM_Init() {
+  BF_Init(LRU);                                                              //Initialize the BF
+  int i;
+  Open_Files = (open_files **)malloc(sizeof(open_files *)*MAXOPENFILES);     //Create array of pointers to open_files
+  for(i=0;i<MAXOPENFILES;i++){
+    Open_Files[i] = NULL;
+  }
+  Scan_Files = (scan_files **)malloc(sizeof(scan_files *)*MAXSCANS);         //Create array of pointers to scan_files
+  for(i=0;i<MAXSCANS;i++){
+    Scan_Files[i] = NULL;                                                    //Initialize each pointert to NULL
+  }
 	return;
 }
 
 
+/*Create a new file based on B+ Tree and initialize the first block with the metadata */
 int AM_CreateIndex(char *fileName, 
 	               char attrType1, 
 	               int attrLength1, 
 	               char attrType2, 
 	               int attrLength2) {
+  int fd;
+  BF_Block *block;
+  BF_Block_Init(&block);                                                          //Initialize Block
+  if((AM_errno = BF_CreateFile(fileName)) != BF_OK){                                 //Create a new Block File
+    BF_Block_Destroy(&block);
+    BF_PrintError(AM_errno);                                                         //If the creation fails
+    return AM_errno;                                                              //Return error
+  }
+
+  if((AM_errno = BF_OpenFile(fileName, &fd)) != BF_OK){                              //Open the file
+    BF_Block_Destroy(&block);
+    BF_PrintError(AM_errno);                                                           //If fails 
+    return AM_errno;                                                                //Return error
+  }
+
+  if((AM_errno = BF_AllocateBlock(fd, block)) != BF_OK){                            //Allocate a new block
+    BF_CloseFile(fd);
+    BF_Block_Destroy(&block);
+    BF_PrintError(AM_errno);                                                           //If fails 
+    return AM_errno;                                                                //Return error
+  } 
+
+  char* data;
+  data = BF_Block_GetData(block);
+  int m = 0;
+  memcpy(data, "B+", sizeof(char)*3);                                               //Initialize the new file as B+ file
+  m+=3;
+  data[3] = attrType1;                                                              //Store the type of key  
+  m += 1;
+  memcpy(data+m,(char *)attrLength1,sizeof(int));                                   //Store the length of key
+  m+=sizeof(int);
+  data[m] = attrType2;                                                              //Store the type second field
+  m+=1;
+  memcpy(data+m,(char *)attrType2,sizeof(int));                                     //Store the length of second field
+  m+=sizeof(int);           
+  memcpy(data+m,"-1",sizeof(int));                                                  //Set root as not exists
+  BF_Block_SetDirty(block);                                                         //Set block Dirty
+  BF_UnpinBlock(block);                                                             //Unpin Block
+  BF_CloseFile(fd);                                                                 //Close file
+  BF_Block_Destroy(&block);
   return AME_OK;
 }
 
@@ -52,9 +113,38 @@ int AM_CloseIndexScan(int scanDesc) {
 
 
 void AM_PrintError(char *errString) {
-  
+  fprintf(stderr,"%s",errString);
+  switch(AM_errno){
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    default:
+      ;
+  }
 }
 
+/*Destroy all the global structures and the BF*/
 void AM_Close() {
-  
+  int i;
+  if(Open_Files != NULL){
+    for(i=0;i<MAXOPENFILES;i++){
+      if(Open_Files[i] != NULL){
+        free(Open_Files[i]->filename);
+        free(Open_Files[i]);
+      }
+    }
+    free(Open_Files);
+  }
+  if(Scan_Files != NULL){
+    for(i=0;i<MAXSCANS;i++){
+      if(Scan_Files[i] != NULL){
+        free(Scan_Files[i]);
+      }
+    }
+    free(Scan_Files);
+  }
+  BF_Close();
 }
