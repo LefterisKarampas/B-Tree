bfb1af67c0161c889893d73faa5ab909884e9d13
LefterisKarampas <mr.lef_21113@hotmail.com>
2017-11-13 23:44:14 +0200

    split && Insert

--- lef/AM_64/src/AM.c
+++ lef/AM_64/src/AM.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include "List.h"
 
 int AM_errno = AME_OK;
 
@@ -213,21 +214,29 @@ int AM_InsertEntry(int fileDesc, void *value1, void *value2) {
   BF_Block *block;
   BF_Block_Init(&block);                                                            //Initialize Block
   if(Open_Files[fileDesc]->root_number == -1){
-    Initialize_Root(fileDesc,value1);                                               //Create B+ Tree's root
+    Initialize_Root(fileDesc,value1,-1,-1);                                         //Create B+ Tree's root
   }
   int block_number;
   int prev = Open_Files[fd]->root_number;
   //Get the data block
+  List * list = List_Create();                                                      //Create a list(Stack) for block's number
   while((block_number = traverse(fileDesc,prev,value1)) != -1){
     prev = block_number;
+    List_Push(list,block_number);                                        //Store the blocks number, in case of splitting
   }
-  int m;
-  BF_GetBlock(Open_Files[fileDesc]->fd,block_number,block);                         //Read block
-  data = BF_Block_GetData(block);
-  int *counter;
-  m = 1;
-  counter = (int *)&data[m];
-
+  prev = List_Pop(list);                                                            //Get the last visited block
+  //Insert new entry and sort it 
+  while(sort(fileDesc,prev,value1,value2) < 0){                                     //If there is not free space
+    Split_Data * split_data = split(fileDesc,prev,value1);                          //SPLIT
+    value2 = split_data->value;                                                     //value2 -> new pointer to index block 
+    int x;
+    x = List_Pop(list);
+    if(x == -1){                                                                    //Split root
+      Initialize_Root(fileDesc,split_data->value,prev,split_data->pointer);
+      break;
+    }
+    prev = x;
+  }
   return AME_OK;
 }
 
@@ -334,6 +343,82 @@ void AM_Close() {
 
 
 
+Split_Data * split(int fileDesc,int block_num,void *value1,void *value2){
+  BF_Block *block;
+  BF_Block_Init(&block);
+  BF_Block *block2;
+  BF_Block_Init(&block2);
+  char* data;
+  BF_GetBlock(fd,block_num,block);                                                  //Read block
+  data = BF_Block_GetData(block);
+  if((AM_errno = BF_AllocateBlock(Open_Files[fileDesc]->fd, block2)) != BF_OK){     //Allocate a new block
+    BF_CloseFile(Open_Files[fileDesc]->fd);
+    BF_Block_Destroy(&block2);
+    return AM_errno;                                                                //Return error
+  } 
+  int new_block;
+  BF_GetBlockCounter(Open_Files[fileDesc]->fd,&new_block);
+  new_block -=1;
+  char* data2;
+  data2 = BF_Block_GetData(block2);
+  data2[0] = data[0];
+  int *counter = &data[1];
+  //Split data block
+  int mid,mod,size,m;
+  int attrLength2;
+  int attrLength1 = Open_Files[fileDesc]->attrLength1;
+
+  //Split data block
+  if(data[0] == 'd'){
+    attrLength2 = Open_Files[fileDesc]->attrLength2;
+    m = sizeof(char) + 3*sizeof(int);
+    size = attrLength1 + attrLength2;
+    mid = ((BF_BLOCK_SIZE-m)/size + 1)/2 + 0.5;                                     //Find the number of the entries that move to new block
+    memcpy(&data[BF_BLOCK_SIZE-sizeof(int)],&new_block,sizeof(int));                //Update pointer to next
+    memcpy(&data2[sizeof(char)+sizeof(int)],&block_num,sizeof(int));                //Update pointer to prev
+  }
+  //Split index block
+  else{
+    attrLength2 = sizeof(int);
+    m = sizeof(char) + 2*sizeof(int);
+    size = attrLength1 + attrLength2;
+    mid = ((BF_BLOCK_SIZE-m)/size + 1.5)/2 + 0.5;                                   //Find the number of the entries that move to new block
+  }
+  mod = (*counter) +1 - mid;
+  memcpy(&data2[1],&mid,sizeof(int));                                               //Update the counter to new block
+  memcpy(&data[1],&mod,sizeof(int));                                                //Update the counter to old block
+  int i;
+  int j = mid-1;
+  int flag = 0;
+  //From the end until the mid, move the entries to new block from the end
+  for(i = *counter-1;i<mid;i--){
+    //If new value is less than current, move the new to other block
+    if(!flag && compare(value,&data[m+j*size],Open_Files[fileDesc]->attrType1,attrLength1)){
+      memcpy(&data2[m+j*size],value1,Open_Files[fileDesc]->attrLength1);                        //Copy new key value
+      memcpy(&data2[m+j*size+attrLength1],value2,attrLength2);                                 
+      flag = 1;
+    }
+    //If new value is bigger than current, move current to other block
+    else{
+      memcpy(&data2[m+j*size],&data[m+i*size],attrLength1);                                     //Copy current key value
+      memcpy(&data2[m+j*size+attrLength1],&data[m+i*size+attrLength1],attrLength2);
+    }
+  }
+  Split_Data * split_data = (Split_Data *)malloc(sizeof(Split_Data));       
+  split_data->pointer = new_block;
+  memcpy(split_data->value,&data2[m],attrLength1);
+  BF_Block_SetDirty(block);                                                         //Set old block Dirty
+  BF_UnpinBlock(block);                                                             //Unpin old Block
+  BF_Block_Destroy(&block);
+  BF_Block_SetDirty(block2);                                                         //Set new block Dirty
+  BF_UnpinBlock(block2);                                                             //Unpin new Block
+  BF_Block_Destroy(&block2);
+  return split_data;                                                                 //Return value for split and new pointer
+}
+
+
+
+
 
 //Compare function for all possible types, return 0 if x1 < x2 else return 1
 int compare(void * value1,void *value2,char attrType,int attrLength){
@@ -363,7 +448,8 @@ int compare(void * value1,void *value2,char attrType,int attrLength){
 
 
 
-//Sort entries of the data block 
+//Sort entries in the block and return 0 if new entry fits in block
+//othewise return -1 for full block, can use it both data block and index block
 int sort(int index,int block_num,void *value1,void *value2){
   BF_Block *block;
   BF_Block_Init(&block);
@@ -371,14 +457,26 @@ int sort(int index,int block_num,void *value1,void *value2){
   BF_GetBlock(fd,block_num,block);                                        //Read block
   data = BF_Block_GetData(block);
   int attrLength1 = Open_Files[index]->attrLength1;                       //Hold attr1 length
-  int attrLength2 = Open_Files[index]->attrLength2;                       //Hold attr1 length
+  int attrLength2;                                                        //Hold attr2 length
   int * counter = (int *)&(data[1]);                                      //Get the counter of entries
+  char id = data[0];
+  if(id == 'd'){
+    attrLength2 = Open_Files[index]->attrLength2;
+    if(*counter >= ((BF_BLOCK_SIZE - 3*sizeof(int)+sizeof(char))/(attrLength1+attrLength2)))
+      return -1;
+  }
+  else{
+    attrLength2 = sizeof(int);
+    if(*counter >= ((BF_BLOCK_SIZE - 2*sizeof(int)+sizeof(char))/(attrLength1+sizeof(int))))
+      return -1;
+  }
   int i;
-  int m = sizeof(char)+sizeof(int);                                       
+  int m = sizeof(char)+2*sizeof(int);                                       
   int flag = 0;
   //From the last entry to the first of block, find the position to insert the new entry
   for(i=*counter-1;i>=0;i++){
-    void * temp = &data[m+(attrLength1+attrLength2)*i];
+    void * temp;
+    temp = &data[m+(attrLength1+attrLength2)*i];
     //Compare the new entry with the current, if new < current then shift right the current
     if(!compare(value1,temp,Open_Files[index]->attrType1,attrLength1)){ 
       memcpy(&data[m+(attrLength1+attrLength2)*(i+1)],(char *)temp,attrLength1);
@@ -398,11 +496,20 @@ int sort(int index,int block_num,void *value1,void *value2){
     memcpy(&data[m],(char *)value1,attrLength1);
     memcpy(&data[m+attrLength1],(char *)value2,attrLength2);
   }
+  *counter = *(counter) + 1;
+  memcpy(&data[1],counter,sizeof(int));                                             //Increase by 1 the counter
+  BF_Block_SetDirty(block);                                                         //Set block Dirty
+  BF_UnpinBlock(block);                                                             //Unpin Block
+  BF_Block_Destroy(block);
+  return 0;
 }
 
-int Initialize_Root(int fileDesc,void * value1){
+
+//Create and Initialize the root of B-Tree,
+//Update the metadata with the new root and the OpenFiles structure
+int Initialize_Root(int fileDesc,void * value1,int p0,int p1){
   BF_Block *block;
-  BF_Block_Init(&block);                                                            //Initialize Block
+  BF_Block_Init(&block);                                                          //Initialize Block
   if((AM_errno = BF_AllocateBlock(Open_Files[fileDesc]->fd, block)) != BF_OK){    //Allocate a new block
     BF_Block_Destroy(&block);
     return AM_errno;                                                              //Return error
@@ -410,25 +517,24 @@ int Initialize_Root(int fileDesc,void * value1){
   int block_num;
   BF_GetBlockCounter(Open_Files[fileDesc]->fd,&block_num)
   block_num -= 1;
+  Open_Files[fileDesc]->root_number = block_num;                                  //Update root from Open files structure
+  data = BF_Block_GetData(block);                                                 //Read block
+  data[0] = 'i';                                                                  //Initialize it as index block
+  int k = 1;
+  int m = 1;
+  memcpy(&data[m],&k,sizeof(int));                                                //Update counter of index block 
+  m += sizeof(int);
+  memcpy(&data[m],&p0,sizeof(int));                                                //Update pointer to NULL
+  m+=sizeof(int);
+  memcpy(&data[m],value1,Open_Files[fileDesc]->attrLength1);                      //Insert entry to root
+  m +=  Open_Files[fileDesc]->attrLength1;
+  memcpy(&data[m],&p1,sizeof(int));
+  BF_Block_SetDirty(block);
+  BF_UnpinBlock(block);
+  //Update the metadata block with the new root block
   BF_GetBlock(Open_Files[fileDesc]->fd,0,block);                                  //Read block
   data = BF_Block_GetData(block); 
   memcpy(&data[13],&block_num,sizeof(int));                                       //Update root tree
-  BF_Block_SetDirty(block);
-  BF_UnpinBlock(block);
-  Open_Files[fileDesc]->root_number = block_num;                                  //Update root from Open files structure
-  BF_GetBlock(Open_Files[fileDesc]->fd,block_num,block);                            //Read block
-  data = BF_Block_GetData(block);
-  data[0] = 'i';
-  int k = 1;
-  int m = 1;
-  memcpy(&data[m],&k,sizeof(int));                                                  //Update counter of index block 
-  k = -1;
-  m += sizeof(int);
-  memcpy(&data[m],&k,sizeof(int));                                                  //Update pointer to NULL
-  m+=sizeof(int);
-  memcpy(&data[m],value1,Open_Files[fileDesc]->attrLength1);                        //Insert entry to root
-  m +=  Open_Files[fileDesc]->attrLength1;
-  memcpy(&data[m],&k,sizeof(int));
   BF_Block_SetDirty(block);
   BF_UnpinBlock(block);
   BF_Block_Destroy(&block);
